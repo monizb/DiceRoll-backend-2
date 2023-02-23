310a696f3cd4ad4964a769adfb5c21c5cdbb08b4
Sumit Paul <sumit.18.paul@gmail.com>
2023-02-24 02:17:07 +0530

    nft template

--- controllers/NFTController.js
+++ controllers/NFTController.js
@@ -6,8 +6,8 @@ exports.generate = [
     // Process request after validation and sanitization.
     async (req, res) => {
         try {
-            const diceResult = req.body.diceResult
-            const html = nft.template({diceResult: diceResult})
+            const diceResults = req.body.diceResult
+            const html = nft.template({diceResults: diceResults})
             res.set('Content-Type', 'text/html');
             res.send(Buffer.from(html));
         }
