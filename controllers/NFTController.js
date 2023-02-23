0dd18d625d50efc5a91b61b42a89635cf14b1fb5
Sumit Paul <sumit.18.paul@gmail.com>
2023-02-24 02:01:32 +0530

    nft template

--- controllers/NFTController.js
+++ controllers/NFTController.js
@@ -1,28 +1,20 @@
 require("dotenv").config();
-const nodeHtmlToImage = require('node-html-to-image')
-const moment = require("moment")
-const path = require('path');
-
 const apiResponse = require("../helpers/apiResponse");
+const nft = require("../helpers/nftTemplate");
 
 exports.generate = [
     // Process request after validation and sanitization.
     async (req, res) => {
-        // try {
-            let fileName = `${moment().utc().format('DD_MM_Y_HH_mm_ss')}.png`
-            let fileLocation = `../nfts/${fileName}`
-            nodeHtmlToImage({
-            output: fileLocation,
-            html: '<html><body>Hello world!</body></html>'
-            })
-            .then(() => console.log('The image was created successfully!'))
-            const absolutePath = path.resolve(fileLocation);
-            apiResponse.successSendImage(res, absolutePath)
-        
-        // }
-        // catch (err) {
-		// 	return apiResponse.ErrorResponse(res, err);
-		// }
+        try {
+            const diceResult = req.body.diceResult
+            const html = nft.template({diceResult: diceResult})
+            apiResponse.successResponseWithData(res, "NFT Generated", html)
+        }
+        catch (err) {
+			return apiResponse.ErrorResponse(res, err);
+		}
     }
 ]
+
+
 
