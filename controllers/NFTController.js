fe5dc8cbd34e90cdf9cbccbb45ec2a35c2642c6d
Sumit Paul <sumit.18.paul@gmail.com>
2023-02-24 02:54:32 +0530

    push

--- controllers/NFTController.js
+++ controllers/NFTController.js
@@ -8,13 +8,6 @@ exports.generate = [
         try {
             const diceResults = req.body.diceResults
             const html = nft.template({diceResults: diceResults})
-            const nodeHtmlToImage = require('node-html-to-image')
-            nodeHtmlToImage({
-            output: './image.png',
-            html: '<html><body>Hello world!</body></html>'
-            })
-            .then(() => console.log('The image was created successfully!'))
-            
             res.set('Content-Type', 'text/html');
             res.send(Buffer.from(html));
         }
